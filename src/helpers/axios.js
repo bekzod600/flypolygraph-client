68be5f70510319064ec37a4ec1c96abc14dc9a3a
bekzod600 <bekzodbaratov334@gmail.com>
2024-02-01 17:09:31 +0500

    fhjsdkls

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://sqmarket-production.up.railway.app/api/v1";
+axios.defaults.baseURL =
+  "https://flypolygraph-server-production.up.railway.app/api/v1";
 // axios.defaults.baseURL = "http://localhost:8080/api/v1";
 
 export default axios;
